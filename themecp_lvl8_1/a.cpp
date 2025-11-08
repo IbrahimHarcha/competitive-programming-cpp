c6ae03ae69e7d6f0ef8ea4d3a857a579a2682e84
IbrahimHarcha <ibrahim.harcha@outlook.fr>
2025-11-08 14:06:12 +0100

    add template for cp

---
+++ themecp_lvl8_1/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define F first
+#define S second
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
